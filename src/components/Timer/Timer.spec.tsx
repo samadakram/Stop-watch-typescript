30dcc10b7508bf084ff6315be145e50e2616bb41
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-26 17:32:29 +0500

    Update Timer.spec.tsx

--- src/components/Timer/Timer.spec.tsx
+++ src/components/Timer/Timer.spec.tsx
@@ -10,4 +10,8 @@ describe("Timer", () => {
   it("should render a <div />", () => {
     expect(container.find("div").length).toBeGreaterThanOrEqual(1)
   });
+
+  it('should render a span', () => {
+    expect(container.find("span").length).toEqual(1)
+  });
 })
