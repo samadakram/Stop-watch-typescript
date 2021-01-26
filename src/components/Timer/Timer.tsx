cdea8cf8496591f5c93025f5a560b565aaff31b0
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-26 17:36:31 +0500

    Update Timer.tsx

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -4,6 +4,7 @@ import './Timer.css';
 const Timer = () => {
     return (
         <div className="timer-container">
+            <h1>Stop Watch</h1>
             <div className="time-box">
                 <span>"00:00:00"</span>
             </div>
