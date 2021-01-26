6296f18f2a20a669545b880ad4a0f626ed45fa44
Abdul Samad <64439944+samadakram@users.noreply.github.com>
2021-01-26 17:07:49 +0500

    Update App.spec.tsx

--- src/components/App/App.spec.tsx
+++ src/components/App/App.spec.tsx
@@ -1,11 +1,17 @@
 import React from 'react';
 import { shallow } from 'enzyme';
 import App from './App';
+import Timer from '../Timer/Timer';
 
 describe('should render App component', () => {
+
     let container = shallow(<App />);
 
     it('should render a div',() => {
         expect(container.find('div').length).toEqual(1);
+    });
+
+    it('should render a timer component instance',() => {
+        expect(container.containsMatchingElement(<Timer />)).toEqual(true)
     });
 });
